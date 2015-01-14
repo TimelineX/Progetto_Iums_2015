451c3b95548d528f183f9e73420f9ef76f0d4803
Mizio <Mizio@Mizio-PC>
2015-01-14 11:23:38 +0100

    Creazione Classe Persona

--- Turnazione/src/it/maugra/data/Persona.java
+++ Turnazione/src/it/maugra/data/Persona.java
@@ -5,5 +5,32 @@ public class Persona {
 	private String Nome;
 	private String Cognome;
 	private String Qualifica;
+	
+	/* Generazione metodi Setter e Getter */ 
+	
+	public String getPerId() {
+		return PerId;
+	}
+	public void setPerId(String perId) {
+		PerId = perId;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCognome() {
+		return Cognome;
+	}
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+	public String getQualifica() {
+		return Qualifica;
+	}
+	public void setQualifica(String qualifica) {
+		Qualifica = qualifica;
+	}
 
 }
